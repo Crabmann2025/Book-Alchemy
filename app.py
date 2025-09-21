65320e474fa78769799389dd3299a82f31d35cf3
Crabmann2025 <1990.c@gmx.de>
2025-09-21 13:53:35 +0200

    Author and book models as well as SQLite database set up

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+# Initialize Flask app
+app = Flask(__name__)
+
+# Set the database URI using an absolute path
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
-
-
-app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
---
+++ data_models.py
@@ -0,0 +1,47 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+db = SQLAlchemy()
+
+class Author(db.Model):
+    """
+    Represents an author in the digital library.
+    """
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_date = db.Column(db.Date, nullable=True)
+    date_of_death = db.Column(db.Date, nullable=True)
+
+    def __repr__(self):
+        return f"<Author {self.name}>"
+
+    def __str__(self):
+        return f"{self.name} ({self.birth_date} - {self.date_of_death})"
+
+
+class Book(db.Model):
+    """
+    Represents a book in the digital library.
+    """
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(20), unique=True, nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
+
+    author = db.relationship('Author', backref=db.backref('books', lazy=True))
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year}) by {self.author.name}"
+
+
+# Create tables in the database
+with app.app_context():
+    db.create_all()
